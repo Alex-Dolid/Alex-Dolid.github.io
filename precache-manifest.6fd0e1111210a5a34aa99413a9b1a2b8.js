f688d716d4743cae46377be031a5d55ccafb6ba4
Alex Dolid <dolid.sasha@gmail.com>
2019-12-14 00:03:44 +0200

    Updates

---
+++ precache-manifest.6fd0e1111210a5a34aa99413a9b1a2b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5cc618a657769285ec7921bd671114",
+    "url": "/first-app-React/index.html"
+  },
+  {
+    "revision": "f8f426e2ad97dd8c7b16",
+    "url": "/first-app-React/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c88cebed7dec8a545eeb",
+    "url": "/first-app-React/static/js/2.250dc4af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/first-app-React/static/js/2.250dc4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8f426e2ad97dd8c7b16",
+    "url": "/first-app-React/static/js/main.4e41c5b8.chunk.js"
+  },
+  {
+    "revision": "5157417d1457f22777be",
+    "url": "/first-app-React/static/js/runtime-main.38e6f125.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-app-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
